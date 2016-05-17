e286e5bb2a4a506a613d43cb9085ddd2251990f3
路亮亮 <luliangliang@luliangliangdeMac-mini.local>
2016-05-17 10:23:24 +0800

    重拍删除图片

--- src/ios/WYVideoCaptureController.m
+++ src/ios/WYVideoCaptureController.m
@@ -542,7 +542,7 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
             _rightTakenPictureCount=0;
         }
         self.title = @"0/6";
-        //[[JRMediaFileManage shareInstance] deleteFileWithEyeType:_isLeftEye];
+        [[JRMediaFileManage shareInstance] deleteFileWithEyeType:_isLeftEye];
         //[wself takePictureMethod];
     }];
     UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
